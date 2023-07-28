161fc128919a77e31fa31c2d43abca4ee51f75d0
cedric <cedricj765@gmail.com>
2023-07-27 21:09:06 -0500

    finished lab

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -7,6 +7,8 @@ function ExampleComponent() {
       <img
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
+        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
     </div>
